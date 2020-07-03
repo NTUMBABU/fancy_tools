27124bca5d21a2a9fdebac25ceb8b43473aa131b
NTUMBABU <tshiteyababci@gmail.com>
2020-07-03 22:43:53 +0400

    modification du fichier istall

--- bin/install.sh
+++ bin/install.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+#echo ". /home/ntumbabu/Documents/ProgramationSCRIPT/cours7/TP/fancy_tools/.aliases" >> ~/.bashrc
+echo ". /home/ntumbabu/Documents/ProgramationSCRIPT/cours7/TP/fancy_tools/fancy_functions.sh" >> ~/.bashrc
 
