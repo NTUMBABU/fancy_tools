8c7948f9e69ad711c6b2fda2e34253dc97948281
NTUMBABU <tshiteyababci@gmail.com>
2020-07-05 01:24:32 +0400

    Modification du fichier install

--- bin/install.sh
+++ bin/install.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-#echo ". /home/ntumbabu/Documents/ProgramationSCRIPT/cours7/TP/fancy_tools/.aliases" >> ~/.bashrc
-echo ". /home/ntumbabu/Documents/ProgramationSCRIPT/cours7/TP/fancy_tools/fancy_functions.sh" >> ~/.bashrc
 
+alias=". $HOME/Documents/ProgramationSCRIPT/cours7/TP/fancy_tools/.aliases" #>> ~/.bashrc
+fanfun=". $HOME/Documents/ProgramationSCRIPT/cours7/TP/fancy_tools/fancy_functions.sh" #>> ~/.bashrc
+barc=$(<~/.bashrc)
+
+if grep -q "$alias" <<< "$barc";
+then
+	echo "Le fichier aliases est deja disponible dans le fichier '.bashrc'"
+else
+	echo $alias >> ~/.bashrc
+	echo 'le fichier aliase est a present dans le .bashrc'
+fi
+
+if grep -q "$fanfun" <<< "$barc";
+then
+	echo "Le fichier fancy functions est deja disponible dans le fichier '.bashrc'"
+else
+	echo $fanfun >> ~/.bashrc
+	echo 'le fichier fancy function est a present dans le .bashrc'
+fi
