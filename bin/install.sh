f4fcc8aaa328b60144c4e861c99c54222c1e4cfe
NTUMBABU <tshiteyababci@gmail.com>
2020-07-05 01:47:50 +0400

    creation du dossier bin et copier du fichier update fancy tools dans le dossier bin

--- bin/install.sh
+++ bin/install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+#creation de variable pour le chemin d'accesse
 alias=". $HOME/Documents/ProgramationSCRIPT/cours7/TP/fancy_tools/.aliases" #>> ~/.bashrc
 fanfun=". $HOME/Documents/ProgramationSCRIPT/cours7/TP/fancy_tools/fancy_functions.sh" #>> ~/.bashrc
 barc=$(<~/.bashrc)
+bin=$"~/bin"
 
+#pour verifier si le fichier aliases est dans le bashrc
 if grep -q "$alias" <<< "$barc";
 then
 	echo "Le fichier aliases est deja disponible dans le fichier '.bashrc'"
@@ -12,6 +15,7 @@ else
 	echo 'le fichier aliase est a present dans le .bashrc'
 fi
 
+#pour verifier si le fichier fancy function exite
 if grep -q "$fanfun" <<< "$barc";
 then
 	echo "Le fichier fancy functions est deja disponible dans le fichier '.bashrc'"
@@ -19,3 +23,30 @@ else
 	echo $fanfun >> ~/.bashrc
 	echo 'le fichier fancy function est a present dans le .bashrc'
 fi
+
+#creation du fichier bin
+if [ -e "$bin" <<< "$HOME" ]
+then
+	cp ~/fancy_tools/bin/updateFancyTools ~/bin/updateFancyTools
+else
+	mkdir ~/bin
+	export PATH=$PATH:$HOME/bin
+	cp ~/fancy_tools/bin/updateFancyTools ~/bin/updateFancyTools
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
